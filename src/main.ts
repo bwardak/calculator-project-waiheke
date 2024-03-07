7c73e887c69e1dbb118b73b7b408d1e2d5ed1d63
bwardak <baheer.wardak06@gmail.com>
2024-03-07 22:13:50 +0000

    Added button for '='. Current test code adds every number input, and pressing equals changes the output on the scren part

--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,62 @@
 import "./style.scss";
-
+                                  // Declaring variables for html elements
 const buttonsNumber =
   document.querySelectorAll<HTMLButtonElement>(".button--number");
 const buttonsEqual =
-  document.querySelector<HTMLButtonElement>(".buttons--equals");
+  document.querySelector<HTMLButtonElement>(".button--equals");
 const screenAnswer = document.querySelector<HTMLDivElement>(".screen--answer");
 
-// const equationAnswer = (event: Event) => {
-//   const answer = event.currentTarget as HTMLDivElement;
-//   answer.innerText = sumOfEquation;
-// };
+const sumOfNumsString: string[] = [];   // String to hold inputted numbers
+let finalAnswer = 0;            // Variable to hold final answer
 
-const sumOfNumsString: string[] = [];
-let finalAnswer = 0;
 
-// if (!buttonsNumber || !buttonsEqual || !screenAnswer || !sumOfNumsString ) {
-//   throw new Error("Issues with Selector");
-// }
-
-const handleClickButtonWithEvent = (event: Event) => {
-  const value = event.currentTarget as HTMLButtonElement;
+const handleClickedButtonNumberToArray = (event: Event) => {    // Takes value of clicked button
+  const value = event.currentTarget as HTMLButtonElement;       // and adds the value to an array
   console.log(value.innerText);
-  sumOfNumsString.push(value.innerText)
-  console.log(sumOfNumsString)
+  sumOfNumsString.push(value.innerText);
+  console.log(sumOfNumsString);
 };
 
-
-const addingNumbers = (): number => {
-  const sumOfNums = sumOfNumsString.filter((e) => typeof e === "string") as number[];
+const addingNumbers = (): number => {                            // Adds all the numbers within array
+  const sumOfNums: number[] = sumOfNumsString.map(Number); // Creates array with number type  
   for (let i: number = 0; i < sumOfNums.length; i++) {
-    finalAnswer += sumOfNums[i]
+    finalAnswer += sumOfNums[i];
   }
-  console.log(sumOfNums)
-  return finalAnswer
-}
+  console.log(finalAnswer);
+  return finalAnswer;
+};
 
 const result = addingNumbers();
-console.log(result)
+console.log(result);
 
-const postingAnswer = (): number => {
-  addingNumbers()
-  let outputAnswer = finalAnswer;
-  console.log(outputAnswer)
-  if (screenAnswer)
-  return screenAnswer.innerText = outputAnswer as string;
-}
-
-
-
-
+const postingAnswer = (): void => {               // Changes the answer section on calc to answer
+  if (screenAnswer) {
+    (screenAnswer.innerText = finalAnswer.toString());
+  } else {
+    (console.error("ERROR"));
+  }
+};
 
 // Multiple event listeners
 
 buttonsNumber.forEach((buttonNumber) => {
-  buttonNumber.addEventListener("click", handleClickButtonWithEvent);
+  buttonNumber.addEventListener("click", handleClickedButtonNumberToArray);
 });
 
 if (buttonsEqual) {
-  buttonsEqual.addEventListener("click", postingAnswer);
+  buttonsEqual.addEventListener("click", () => {
+    addingNumbers();
+    postingAnswer();
+  });
 }
-
-if (screenAnswer){
-  screenAnswer.addEventListener("click", postingAnswer)
-}
-
-
-
 
 
 
 // Button clicks when clicking middle
 // Makes the buttons only add the numbers and the rest get ommitted
+// for loop for order of operations, [i] of % -1 + 1
+// Put numbers in array, turn numbers around %/X into numbers and then perform task
+// Alternatively use REGEX 
+// - First find numbers in between brackets, then befre '^' (exponential), then before/after 
+//   X/%, then before/after +/-
+// - Adds each to an array per category, a
